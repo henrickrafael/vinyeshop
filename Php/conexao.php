775cefcf0a930d0a26a4b9bf3510155d557a6933
eshimas <marceleshima@gmail.com>
2022-06-05 09:53:55 -0300

    Inserts tela de cadastro e ajuste de auteticacao tela Login

--- Php/conexao.php
+++ Php/conexao.php
@@ -2,10 +2,10 @@
 
 	$host = 'localhost';
 	$usr = 'root';
-	$pwd = 'nirvana123';
+	$pwd = '';
 	$db = 'vinyeshop';
 	$port = 3306;
 	$socket = '/var/run/mysqld/mysqld.sock';
 
-	$con = mysqli_connect($host, $usr, $pwd, $db, $port, $socket);
+	$con = mysqli_connect($host, $usr, $pwd, $db);
 ?>
